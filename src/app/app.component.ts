efdbb679889a051c1f78aa6d8d57d1620246eb96
PedroHBaron <tecnologia2@conceitocontabilidade.com.br>
2026-03-11 17:10:05 -0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { ListaLivrosComponent } from './componentes/lista-livros/lista-livros.component';
+import { GeneroLiterarioComponent } from './componentes/genero-literario/genero-literario.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CabecalhoComponent, RodapeComponent, ListaLivrosComponent, GeneroLiterarioComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'organo';
+}
---
+++ src/app/componentes/livro/livro.component.ts
@@ -0,0 +1,18 @@
+import { Component, input } from '@angular/core';
+import { Livro } from './Livro';
+
+@Component({
+  selector: 'app-livro',
+  standalone: true,
+  imports: [],
+  templateUrl: './livro.component.html',
+  styleUrl: './livro.component.css'
+})
+export class LivroComponent {
+
+  livro = input.required<Livro>();
+  alternarFavorito() {
+    this.livro().favorito = !this.livro().favorito;
+  }
+
+}
